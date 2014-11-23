00babf9833836e167cf325e7ae931dae072c1ea2
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-11-23 09:09:56 -0800

    adding alamofire and swiftjson "

---
+++ TodayNBANews/SCSwiftFramework/SCSwiftFramework/SCSwiftFramework.h
@@ -0,0 +1,17 @@
+//
+//  SCSwiftFramework.h
+//  SCSwiftFramework
+//
+//  Created by Shengzhe Chen on 11/22/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCSwiftFramework.
+FOUNDATION_EXPORT double SCSwiftFrameworkVersionNumber;
+
+//! Project version string for SCSwiftFramework.
+FOUNDATION_EXPORT const unsigned char SCSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCSwiftFramework/PublicHeader.h>
---
+++ TodayNBANews/TodayNBANews/Controller/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TodayNBANews
+//
+//  Created by Shengzhe Chen on 11/22/14.
+//  Copyright (c) 2014 Shengzhe Chen. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "http://httpbin.org/get", parameters: ["foo": "bar"])
+            .response { (request, response, data, error) in
+                println(request)
+                println(response)
+                println(error)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
